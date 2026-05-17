b5aa2093ad50845506508657f4fd775bd0ad19e3
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:06:15 -0700

    scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+Use this exact code in `10-ill_use_my_scale.py`:
+
+```python id="b6y0q1"
+#!/usr/bin/env python3
+"""Calculate the shape of a numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray."""
+    return matrix.shape
+```
+
