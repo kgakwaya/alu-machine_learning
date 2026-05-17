2f2dd6106bd70abbe9bf70afdeab9f35cb0ac4d3
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:50:13 -0700

    squashed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
-"""Concatenate two matrices along a specific axis."""
+"""Concatenate matrices along a given axis."""
 
 
 def cat_matrices(mat1, mat2, axis=0):
     """Concatenates two matrices or returns None if impossible."""
 
+    # If types differ, cannot concatenate
     if type(mat1) != type(mat2):
         return None
 
-    if isinstance(mat1, list):
-        if axis == 0:
-            return mat1 + mat2
+    # If we are at scalar level
+    if not isinstance(mat1, list):
+        return mat1
 
-        if len(mat1) != len(mat2):
+    # axis 0: direct concatenation
+    if axis == 0:
+        return mat1 + mat2
+
+    # must have same length to go deeper
+    if len(mat1) != len(mat2):
+        return None
+
+    # recursive step
+    result = []
+    for i in range(len(mat1)):
+        merged = cat_matrices(mat1[i], mat2[i], axis - 1)
+        if merged is None:
             return None
+        result.append(merged)
 
-        return [
-            cat_matrices(mat1[i], mat2[i], axis - 1)
-            for i in range(len(mat1))
-        ]
-
-    return mat1
+    return result
