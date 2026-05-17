f521436424e3ed6a95073f21db781c19dbef4ee0
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:20:22 -0700

    scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,12 +1,7 @@
-Use this exact code in `10-ill_use_my_scale.py`:
-
-```python id="b6y0q1"
 #!/usr/bin/env python3
-"""Calculate the shape of a numpy.ndarray."""
+"""Return the shape of a numpy.ndarray."""
 
 
 def np_shape(matrix):
-    """Returns the shape of a numpy.ndarray."""
+    """Calculates the shape of a numpy.ndarray."""
     return matrix.shape
-```
-
