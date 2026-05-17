cb5565c299d104bff32d1c693fcf1b910d61892d
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 20:20:24 +0200

    squashed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -2,46 +2,39 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """
-    Concatenates two matrices along a given axis.
-    Returns a new matrix or None if invalid.
+    Concatenates two matrices along a specific axis.
+
+    Returns a new matrix if possible, otherwise None.
     """
 
-    def get_shape(m):
-        """Returns shape of matrix, or None if irregular."""
+    def shape(m):
         if not isinstance(m, list):
             return ()
         if len(m) == 0:
             return (0,)
-        first_shape = get_shape(m[0])
-        if first_shape is None:
-            return None
-        for item in m:
-            if get_shape(item) != first_shape:
+        base = shape(m[0])
+        for x in m:
+            if shape(x) != base:
                 return None
-        return (len(m),) + first_shape
+        return (len(m),) + base
 
     def concat(a, b, ax):
         if ax == 0:
             return a + b
-        return [
-            concat(a[i], b[i], ax - 1)
-            for i in range(len(a))
-        ]
+        return [concat(a[i], b[i], ax - 1) for i in range(len(a))]
 
-    shape1 = get_shape(mat1)
-    shape2 = get_shape(mat2)
+    s1 = shape(mat1)
+    s2 = shape(mat2)
 
-    if shape1 is None or shape2 is None:
+    if s1 is None or s2 is None:
+        return None
+    if len(s1) != len(s2):
+        return None
+    if axis < 0 or axis >= len(s1):
         return None
 
-    if len(shape1) != len(shape2):
-        return None
-
-    if axis < 0 or axis >= len(shape1):
-        return None
-
-    for i in range(len(shape1)):
-        if i != axis and shape1[i] != shape2[i]:
+    for i in range(len(s1)):
+        if i != axis and s1[i] != s2[i]:
             return None
 
     return concat(mat1, mat2, axis)
