765556c92c9bbea2f11d3471a4ffa23478a61688
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:23:13 -0700

    exchange

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,3 @@
-Use this exact code in `11-the_western_exchange.py`:
-
-```python id="q3m8vx"
 #!/usr/bin/env python3
 """Transpose a numpy.ndarray."""
 
@@ -8,5 +5,3 @@ Use this exact code in `11-the_western_exchange.py`:
 def np_transpose(matrix):
     """Returns the transpose of a numpy.ndarray."""
     return matrix.transpose()
-```
-
