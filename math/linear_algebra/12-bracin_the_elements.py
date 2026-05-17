c6d0eff002bb3d0a0d868938c027ca7d3c5cbef9
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:25:49 -0700

    elements

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,6 +1,3 @@
-Use this exact code in `12-bracin_the_elements.py`:
-
-```python id="d2k7mw"
 #!/usr/bin/env python3
 """Perform element-wise operations on numpy.ndarrays."""
 
@@ -8,5 +5,3 @@ Use this exact code in `12-bracin_the_elements.py`:
 def np_elementwise(mat1, mat2):
     """Returns element-wise sum, difference, product, and quotient."""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
-```
-
