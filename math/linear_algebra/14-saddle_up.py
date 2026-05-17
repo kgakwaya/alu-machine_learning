916813101918a8848402ffdb653658f507e16bc3
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 08:15:06 -0700

    saddle

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+Use this exact code in `14-saddle_up.py`:
+
+```python id="m8t2qv"
+#!/usr/bin/env python3
+"""Perform matrix multiplication using numpy."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the matrix product of two numpy.ndarrays."""
+    return np.matmul(mat1, mat2)
+```
+
