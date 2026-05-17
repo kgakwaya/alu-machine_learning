cffc6c5cbbeaf6ac6ab782b85116c87882422769
kgakwaya <k.gakwaya@alustudent.com>
2026-05-17 19:25:38 +0200

    squashed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -3,25 +3,25 @@
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """Concatenates two matrices or returns 'OK' if impossible."""
+    """Concatenates two matrices or returns None if impossible."""
 
     if type(mat1) != type(mat2):
-        return "OK"
+        return None
 
     if not isinstance(mat1, list):
-        return mat1
+        return mat1 + mat2
 
     if axis == 0:
         return mat1 + mat2
 
     if len(mat1) != len(mat2):
-        return "OK"
+        return None
 
     result = []
     for i in range(len(mat1)):
         merged = cat_matrices(mat1[i], mat2[i], axis - 1)
-        if merged == "OK":
-            return "OK"
+        if merged is None:
+            return None
         result.append(merged)
 
     return result
