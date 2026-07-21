a4814694e014824f063c1c661d0fd06bc3c81626
kgakwaya <k.gakwaya@alustudent.com>
2026-07-21 02:58:21 -0700

    pool

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
-"""Stacked bar graph showing the number of fruit each person possesses."""
+'''
+    a function def
+    pool(images, kernel_shape, pool_shape, mode='max'):
+    that performs a pooling on images:
+    mode: max or avg
+'''
+
+
 import numpy as np
-import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
 
-people = ['Farrah', 'Fred', 'Felicia']
-fruit_names = ['apples', 'bananas', 'oranges', 'peaches']
-colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+def pool(images, kernel_shape, stride, mode='max'):
+    '''
+        images: numpy.ndarray with shape (m, h, w, c)
+            m: number of images
+            h: height in pixels
+            w: width in pixels
+            c: number of channels
+        kernel_shape: tuple of (kh, kw)
+            kh: height of the kernel
+            kw: width of the kernel
+        stride: tuple of (sh, sw)
+            sh: stride for the height of the image
+            sw: stride for the width of the image
+        mode: max or avg
+        Returns: numpy.ndarray containing the pooled images
+    '''
+    m, height, width, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
 
-bottom = np.zeros(3)
-for i in range(4):
-    plt.bar(people, fruit[i], width=0.5, bottom=bottom,
-            color=colors[i], label=fruit_names[i])
-    bottom += fruit[i]
+    ph = ((height - kh) // sh) + 1
+    pw = ((width - kw) // sw) + 1
+    pooled = np.zeros((m, ph, pw, c))
 
-plt.ylabel('Quantity of Fruit')
-plt.title('Number of Fruit per Person')
-plt.ylim(0, 80)
-plt.yticks(range(0, 81, 10))
-plt.legend()
-plt.show()
+    for i, h in enumerate(range(0, (height - kh + 1), sh)):
+        for j, w in enumerate(range(0, (width - kw + 1), sw)):
+            if mode == 'max':
+                output = np.max(images[:, h:h + kh, w:w + kw, :], axis=(1, 2))
+            elif mode == 'avg':
+                output = np.average(images[:, h:h + kh, w:w + kw, :],
+                                    axis=(1, 2))
+            else:
+                pass
+            pooled[:, i, j, :] = output
+
+    return pooled
